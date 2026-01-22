b74e289d594cd939b6dc90d5036b92317ce85e8b
lukamikautadze <92504466+lukamikautadze@users.noreply.github.com>
2026-01-22 18:22:51 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,32 @@
-const playlist = [
+const CACHE_NAME = 'molscreen-v3';
+const ASSETS = [
+  './index.html',
+  // აქ ჩაამატე შენი Dropbox-ის პირდაპირი ლინკები (dl=1)
 'https://www.dropbox.com/scl/fi/p3jnpcnq5zq0i00y423lj/.mp4?rlkey=c87u2tmn8amluocftqtn792cl&st=d1wh4tew&raw=1',
 'https://www.dropbox.com/scl/fi/v2bx6rptchmopoyj2wg84/.mp4?rlkey=a8d5xdh97r1k0b5y1p06mmvkr&st=05anps8s&raw=1'
 ];
 
-// ფუნქცია, რომელიც ვიდეოს აქცევს "ლოკალურ ფაილად"
-async function getLocalVideoUrl(url) {
-    const cache = await caches.open('molscreen-v2');
-    let response = await cache.match(url);
-    
-    // თუ ქეშში არ არის, ინტერნეტიდან მოაქვს
-    if (!response) {
-        response = await fetch(url);
-        cache.put(url, response.clone());
-    }
-    
-    const blob = await response.blob();
-    return URL.createObjectURL(blob);
-}
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('ვიდეოები იწერება მეხსიერებაში...');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
 
-async function prepareNext(player, index) {
-    try {
-        const localUrl = await getLocalVideoUrl(playlist[index]);
-        player.src = localUrl;
-        player.load();
-    } catch (e) {
-        console.error("ოფლაინ ჩატვირთვის შეცდომა", e);
-    }
-}
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+  }));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
