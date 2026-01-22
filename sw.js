374d27e3fbb64f4d7612f975e2edb9c4df43a241
lukamikautadze <92504466+lukamikautadze@users.noreply.github.com>
2026-01-22 17:57:10 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'molscreen-v2'; // სახელი შევცვალოთ (v2), რომ ბრაუზერმა განახლება დაინახოს
+const CACHE_NAME = 'molscreen-cache-v3';
 const urlsToCache = [
   './index.html',
   'https://www.dropbox.com/scl/fi/p3jnpcnq5zq0i00y423lj/.mp4?rlkey=c87u2tmn8amluocftqtn792cl&st=d1wh4tew&raw=1',
@@ -6,20 +6,23 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
-  // skipWaiting აიძულებს ბრაუზერს მაშინვე გამოიყენოს ახალი ვერსია
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Caching videos...');
+      console.log('Downloading videos to cache...');
       return cache.addAll(urlsToCache);
     })
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      // თუ ფაილი ქეშშია, იქიდან აიღებს, თუ არა - ინტერნეტიდან
+      // აბრუნებს ფაილს ქეშიდან, თუ არადა მიდის ინტერნეტში
       return response || fetch(event.request);
     })
   );
