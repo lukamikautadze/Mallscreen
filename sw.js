6cfbb79015e442e121b27da309dddad0cf71777c
lukamikautadze <92504466+lukamikautadze@users.noreply.github.com>
2026-01-22 17:17:40 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = 'molscreen-v1';
 const urlsToCache = [
   './index.html',
   // აქ ჩაამატე შენი ვიდეოს ლინკები, რომ ბრაუზერმა დაიმახსოვროს
-  'https://www.dropbox.com/scl/fi/fdey9od9eii498lsanwt6/Video-lyriced-by-kirkitaa7-music-lyricedits-k...-7594790642086087943.mp4?rlkey=5f5bv46xlacc7wrr0rgjqlnis&st=ooubh7e6&dl=0',
-  'https://www.dropbox.com/scl/fi/15wx1gsxro91i7dnv42wz/KFC.mp4?rlkey=e6sn7glqwf2hl4bsqb1eve0f0&st=15baqphh&dl=0'
+  'https://www.dropbox.com/scl/fi/fdey9od9eii498lsanwt6/Video-lyriced-by-kirkitaa7-music-lyricedits-k...-7594790642086087943.mp4?rlkey=5f5bv46xlacc7wrr0rgjqlnis&st=ooubh7e6&raw=1',
+  'https://www.dropbox.com/scl/fi/15wx1gsxro91i7dnv42wz/KFC.mp4?rlkey=e6sn7glqwf2hl4bsqb1eve0f0&st=15baqphh&raw=1'
 ];
 
 self.addEventListener('install', event => {
