fceb378a1c9a89bc8b86b29d6d6149256dcf5b5e
lukamikautadze <92504466+lukamikautadze@users.noreply.github.com>
2026-01-22 18:14:05 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-const CACHE_NAME = 'molscreen-cache-v2.4';
-const urlsToCache = [
-  './index.html',
-  'https://www.dropbox.com/scl/fi/p3jnpcnq5zq0i00y423lj/.mp4?rlkey=c87u2tmn8amluocftqtn792cl&st=d1wh4tew&raw=1',
-  'https://www.dropbox.com/scl/fi/v2bx6rptchmopoyj2wg84/.mp4?rlkey=a8d5xdh97r1k0b5y1p06mmvkr&st=05anps8s&raw=1'
+const playlist = [
+'https://www.dropbox.com/scl/fi/p3jnpcnq5zq0i00y423lj/.mp4?rlkey=c87u2tmn8amluocftqtn792cl&st=d1wh4tew&raw=1',
+'https://www.dropbox.com/scl/fi/v2bx6rptchmopoyj2wg84/.mp4?rlkey=a8d5xdh97r1k0b5y1p06mmvkr&st=05anps8s&raw=1'
 ];
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Downloading videos to cache...');
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// ფუნქცია, რომელიც ვიდეოს აქცევს "ლოკალურ ფაილად"
+async function getLocalVideoUrl(url) {
+    const cache = await caches.open('molscreen-v2');
+    let response = await cache.match(url);
+    
+    // თუ ქეშში არ არის, ინტერნეტიდან მოაქვს
+    if (!response) {
+        response = await fetch(url);
+        cache.put(url, response.clone());
+    }
+    
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+}
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      // აბრუნებს ფაილს ქეშიდან, თუ არადა მიდის ინტერნეტში
-      return response || fetch(event.request);
-    })
-  );
-});
+async function prepareNext(player, index) {
+    try {
+        const localUrl = await getLocalVideoUrl(playlist[index]);
+        player.src = localUrl;
+        player.load();
+    } catch (e) {
+        console.error("ოფლაინ ჩატვირთვის შეცდომა", e);
+    }
+}
