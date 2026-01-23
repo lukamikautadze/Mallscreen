c591bdb5cd95321371501aa2a52b18aabaab71c8
lukamikautadze <92504466+lukamikautadze@users.noreply.github.com>
2026-01-23 04:25:31 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'molscreen-v6.0';
 // List of assets to cache for offline use
 const ASSETS = [
   './index.html',
-  'https://www.dropbox.com/scl/fi/p3jnpcnq5zq0i00y423lj/.mp4?rlkey=c87u2tmn8amluocftqtn792cl&st=d1wh4tew&raw=1',
-  'https://www.dropbox.com/scl/fi/v2bx6rptchmopoyj2wg84/.mp4?rlkey=a8d5xdh97r1k0b5y1p06mmvkr&st=05anps8s&raw=1'
+  'გოჩიტ.mp4',
+  'კფცცც.mp4'
 ];
 
 // 1. Installation: Download all assets into the cache
